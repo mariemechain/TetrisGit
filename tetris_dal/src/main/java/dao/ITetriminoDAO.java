9aff454702bc91c65fb0b0312177ddcbf7778a5f
Unknown <mariealizee.mechain@gmail.com>
2018-01-19 11:56:08 +0100

    Merge branch 'Marie' into Dev
    MISE A NIVEAU

--- tetris_dal/src/main/java/dao/ITetriminoDAO.java
+++ tetris_dal/src/main/java/dao/ITetriminoDAO.java
@@ -1,6 +1,7 @@
 package dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import model_tetrimino.Tetrimino;
 
 public interface ITetriminoDAO extends JpaRepository <Tetrimino, Integer> {
 
